dfb222230f9eb7d78faa5c15e52b0d2f2c6b2f30
glindor <blaquee@gmail.com>
2016-08-06 21:18:23 -0400

    init project

---
+++ LdrDllNotification/dllmain.cpp
@@ -0,0 +1,118 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "ntapi.h"
+
+
+_LdrRegisterDllNotification LdrRegisterDllNotification = NULL;
+_LdrUnregisterDllNotification LdrUnregisterDllNotifcation = NULL;
+PVOID Cookie = NULL;
+FILE *fp = NULL;
+
+BOOL GetNtFunctions()
+{
+	HMODULE hNtDll;
+	if (!(hNtDll = GetModuleHandle(TEXT("ntdll.dll"))))
+	{
+		return FALSE;
+	}
+	LdrRegisterDllNotification = (_LdrRegisterDllNotification)GetProcAddress(hNtDll, "LdrRegisterDllNotification");
+	LdrUnregisterDllNotifcation = (_LdrUnregisterDllNotification)GetProcAddress(hNtDll, "LdrUnregisterDllNotification");
+	if (!LdrRegisterDllNotification || !LdrUnregisterDllNotifcation)
+		return FALSE;
+
+	return TRUE;
+}
+
+void CALLBACK MyDllNotification(ULONG Reason, PLDR_DLL_NOTIFICATION_DATA NotificationData,
+	PVOID Context)
+{
+	//WCHAR DllName[256];
+	//Check for the reason
+	switch (Reason)
+	{
+	//LDR_DLL_NOTIFICATION_REASON_LOADED
+	case LDR_DLL_NOTIFICATION_REASON_LOADED:
+		break;
+	//LDR_DLL_NOTIFICATION_REASON_UNLOADED
+	case LDR_DLL_NOTIFICATION_REASON_UNLOADED:
+	{
+		
+		/* A Dll has been unloaded from memory, grab the unloaded strcture information
+		PLDR_DLL_UNLOADED_NOTIFICATION_DATA UnloadedData = (PLDR_DLL_UNLOADED_NOTIFICATION_DATA)malloc(sizeof(LDR_DLL_UNLOADED_NOTIFICATION_DATA));
+		memset(UnloadedData, 0, sizeof(LDR_DLL_UNLOADED_NOTIFICATION_DATA));
+		RtlCopyMemory(&UnloadedData, &NotificationData->Unloaded, sizeof(LDR_DLL_UNLOADED_NOTIFICATION_DATA));
+		*/
+
+		wchar_t message[256] = { 0 };
+		swprintf(message, L"DLL was unloaded event for %wZ\n", &NotificationData->Unloaded.FullDllName);
+		MessageBoxW(0, L"Event", message, MB_OK);
+		fprintf_s(fp, "Dll Unloaded Event triggered for: %wZ\n", &NotificationData->Unloaded.FullDllName);
+
+		//RtlCopyMemory(DllName, UnloadedData.FullDllName->Buffer, min(UnloadedData.FullDllName->Length, 256));
+		//Null terminate the string (UNSAFE)
+		//DllName[min(UnloadedData.FullDllName->Length, 256) + 1] = '\0';
+		
+	}
+	default:
+		return;
+	}
+}
+
+
+BOOL Startup()
+{
+	NTSTATUS ret;
+	_wfopen_s(&fp, L"C:\\Log\\logger.txt", L"wb");
+	if (!GetNtFunctions())
+		return FALSE;
+	if (fp)
+	{
+		if (LdrRegisterDllNotification) 
+		{
+			ret = LdrRegisterDllNotification(0, &MyDllNotification, NULL, &Cookie);
+			if (ret != STATUS_SUCCESS)
+				return FALSE;
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+void Cleanup()
+{
+	if (LdrUnregisterDllNotifcation)
+		if (Cookie != NULL)
+			LdrUnregisterDllNotifcation(Cookie);
+	return;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		if (!Startup())
+			break;
+
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+	{
+		Cleanup();
+		break;
+
+	}
+	}
+	return TRUE;
+}
+
